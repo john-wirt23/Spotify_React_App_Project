7ff92f09f388f1ec44f512c01861072473387ce7
John Wirt <john.wirt23@gmail.com>
2023-05-26 16:36:25 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MyFunction from './MyFunction';
+
+function App() {
+  return (
+    <div className="App">
+      <MyFunction/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MyFunction.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function MyFunction () {
+    return <p>Hello</p>;
+};
+
+export default MyFunction;
